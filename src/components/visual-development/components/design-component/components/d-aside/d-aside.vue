dcb6c63e967511a9d290fefdfc71ca51538f13a1
vsdeeper <vsdeeper@qq.com>
2024-02-27 17:30:59 +0800

    feat: remove ProjectNameEditor

--- src/components/visual-development/components/design-component/components/d-aside/d-aside.vue
+++ src/components/visual-development/components/design-component/components/d-aside/d-aside.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { MergeDesignData } from '@/components';
-import { IdEditor, ProjectNameEditor, WidthEditor } from '../property-editor';
-import { isRootComponent } from '@/components/visual-development/util';
-import { useGlobal } from '@/stores';
+import { IdEditor, WidthEditor } from '../property-editor';
 import { ROW_GUTTER } from '../constants';
 
 defineProps<{
@@ -14,9 +12,6 @@ defineProps<{
   <el-row :gutter="ROW_GUTTER">
     <ResponsiveCol>
       <IdEditor :form-data="formData"></IdEditor>
-    </ResponsiveCol>
-    <ResponsiveCol v-if="isRootComponent(formData.id, useGlobal().designData)">
-      <ProjectNameEditor :form-data="formData"></ProjectNameEditor>
     </ResponsiveCol>
     <ResponsiveCol>
       <WidthEditor :options="formData.options"></WidthEditor>
--- src/components/visual-development/components/design-component/components/property-editor/project-name-editor/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ProjectNameEditor } from './project-name-editor.vue'
