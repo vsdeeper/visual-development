8a59e58449fcba6c5d4929f34ebe86e360cfdd41
chenshuai1 <chenshuai1@zonst.cn>
2024-06-22 15:18:17 +0800

    feat: revert type

--- src/components/visual-development/index.ts
+++ src/components/visual-development/index.ts
@@ -16,23 +16,20 @@ export { default as VisualDevelopment, type AddComponentInstance } from './visua
 export type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
 
 /** 设计组件的选项配置类型 */
-// export type DesignDataOptions = ProjectDesignDataOptions &
-//   ViewDesignDataOptions &
-//   SearchDesignDataOptions &
-//   TableDesignDataOptions
-export type DesignDataOptions = Record<string, any>
+export type DesignDataOptions = ProjectDesignDataOptions &
+  ViewDesignDataOptions &
+  SearchDesignDataOptions &
+  TableDesignDataOptions
 
 /** 当前配置数据类型 */
-// export type ActiveDesignData =
-//   | ProjectDesignData
-//   | ViewDesignData
-//   | SearchDesignData
-//   | TableDesignData
-export type ActiveDesignData = Record<string, any>
+export type ActiveDesignData =
+  | ProjectDesignData
+  | ViewDesignData
+  | SearchDesignData
+  | TableDesignData
 
 /** 设计数据的合并类型 */
-// export type MergeDesignData = ProjectDesignData &
-//   ViewDesignData &
-//   SearchDesignData &
-//   TableDesignData
-export type MergeDesignData = Record<string, any>
+export type MergeDesignData = ProjectDesignData &
+  ViewDesignData &
+  SearchDesignData &
+  TableDesignData
